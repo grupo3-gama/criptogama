3db3bd9615947cf9cbb7f9c6a25037a9d3540883
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-07-09 13:13:23 -0300

    att
    att

---
+++ src/Components/css/Descricao/Descricao.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled, {keyframes} from 'styled-components'
+import '../../css/Descrip.css'
+import DescTexto from './Parts/DescTexto'
+import Grafico from './Parts/Grafico'
+
+class descricao extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            tab: "descricao"
+        }
+    }
+
+    render(){
+
+        const ColorAnimation = keyframes`
+        0%  {border-color: #631ED6;}
+        50% {border-color: #5FD43C;}
+        100% {border-color: #631ED6;}
+    `
+
+        const Seletor = styled.button`
+        color: #fff;
+        font-size: 20px;
+        font-weight: bold;
+        background-color: transparent;
+        border: none;
+        margin: 0 10px 0 0;
+        border-bottom: 2px solid #fff;
+        transition: .5s;
+        &:hover, &.active {
+            animation: ${ColorAnimation} 3s infinite alternate;
+        }
+        &:focus {
+            outline: none;
+        }
+    `
+
+    
+
+        return(
+            <div className='desc-content'>
+                <div className='seletor'>
+                    <Seletor className={this.state.tab === "descricao" ? "active" : ""} onClick={() => this.setState({tab: "descricao"})}>
+                        Descricao
+                    </Seletor>
+                    <Seletor className={this.state.tab === "grafico" ? "active" : ""} onClick={() => this.setState({tab: "grafico"})}>
+                        Gráfico
+                    </Seletor>
+                    <Seletor className={this.state.tab === "skills" ? "active" : ""} onClick={() => this.setState({tab: "skills"})}>
+                        Trabalhos
+                    </Seletor>
+                </div>
+                <div className='desc-text'>
+                    <div
+                    className='text-item'
+                    style={{display: this.state.tab === 'descricao' ? "block" : "none"}}
+                    >
+                        <DescTexto />
+                    </div>
+                    <div
+                    className='text-item'
+                    style={{display: this.state.tab === 'grafico' ? "block" : "none"}}
+                    >
+                        <Grafico    />
+                    </div>
+                    <div
+                    className='text-item'
+                    style={{display: this.state.tab === 'skills' ? "block" : "none"}}
+                    >
+                        Skills
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default descricao
--- src/Pages/MoedaPage.js
+++ src/Pages/MoedaPage.js
@@ -1,35 +1,14 @@
-import axios from 'axios';
-import React, { useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import { CryptoState } from '../Config/Mudarmoeda';
-import { MoedaUnica } from '../Config/Api'
+import React from 'react'
 import './css/pages.css'
+import Particles from 'react-tsparticles'
+import { loadFull } from "tsparticles";
+import Sobre from '../Sections/SobreaMoeda/Sobre';
 
 const MoedaPage = () => {
 
-  const { id } = useParams();
-  const [coin, setCoin] = useState();
-
-  const {currency, symbol} = CryptoState();
-
-  const fetchCoin = async () => {
-    const { data } = await axios.get(MoedaUnica(id));
-    setCoin(data);
-  }
-
-  useEffect(() =>{
-    fetchCoin();
-  },[]);
-
   return (
     <div className='moedapage-content'>
-      <div className='moedapage-side'>
-        <img
-        src={coin?.image.large}
-        alt={coin?.name}
-        />
-      </div>
-      <h2>{(coin?.description.en.split(". ")[0])}.</h2>
+      <Sobre  />
     </div>
   )
 }
