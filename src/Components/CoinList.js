bd2b5c685646580e1ec2d2291df951cd4610853f
Lucas <lucascardim777@gmail.com>
2022-07-09 18:18:23 -0300

    CoinList component almost done

--- src/Components/CoinList.js
+++ src/Components/CoinList.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import coinGecko from '../apis/coinGecko';
 import { WatchListContext } from '../context/watchlist';
+import Coin from './Coin';
 
 const CoinList = () => {
   const [coins, setCoins] = useState([]);
-  const {watchlist} = useContext(WatchListContext);
+  const { watchlist } = useContext(WatchListContext);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchApiData = async () => {
@@ -18,7 +19,19 @@ const CoinList = () => {
     };
     fetchApiData();
   }, []);
-  return <div>CoinList</div>;
+
+  const coinRenderer = () => {
+    if (loading) return <div>Loading Data</div>;
+
+    return (
+      <ul className='coinlist list-group mt-2'>
+        {coins.map(coin => {
+          return <Coin key={coin.id} coin={coin} />;
+        })}
+      </ul>
+    );
+  };
+  return <div>{coinRenderer()}</div>;
 };
 
 export default CoinList;
--- src/Pages/CoinSummary.js
+++ src/Pages/CoinSummary.js
@@ -4,7 +4,7 @@ import Coins from '../Components/Coins';
 
 const CoinSummary = () => {
   return (
-    <div>
+    <div className="coinsummary shadow border p-2 rounded mt-2 bg-light">
       <Coins />
       <CoinList />
     </div>
