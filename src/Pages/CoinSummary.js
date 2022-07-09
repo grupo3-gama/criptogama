f2a8e18c834717282a44e3fe3bd3220ae5e5b4bc
Lucas <lucascardim777@gmail.com>
2022-07-09 16:45:14 -0300

    adding two new components and setting them on CoinSummary page

--- src/Pages/CoinSummary.js
+++ src/Pages/CoinSummary.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import CoinList from '../Components/CoinList';
+import Coins from '../Components/Coins';
 
 const CoinSummary = () => {
   return (
     <div>
-      
+      <Coins />
+      <CoinList />
     </div>
   )
 }
+export default CoinSummary;
