f5421a16cae570959372399dfc8021c342f065f8
Lucas <lucascardim777@gmail.com>
2022-07-09 17:09:03 -0300

    setting the base URL for coingecko, making a fetch on the api on CoinList component

--- src/Components/CoinList.js
+++ src/Components/CoinList.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import coinGecko from '../apis/coinGecko';
 
 const CoinList = () => {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    const fetchApiData = async () => {
+      const response = await coinGecko.get('/coins/markets', {
+        vs_currency: 'usd',
+        ids: 'bitcoin, ethereum',
+      });
+    };
+    fetchApiData();
+  }, []);
   return <div>CoinList</div>;
 };
 
