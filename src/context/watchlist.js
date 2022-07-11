3cc5555ba972947c9a85c6d60fbd2cf5449b12e3
Lucas <lucascardim777@gmail.com>
2022-07-11 20:09:36 -0300

    local storage implemented

--- src/context/watchlist.js
+++ src/context/watchlist.js
@@ -1,12 +1,22 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const WatchListContext = createContext();
 
 export const WatchListProvider = props => {
-  const [watchlist, setWatchList] = useState(['bitcoin', 'ethereum', 'theter']);
+  const [watchlist, setWatchList] = useState(
+    localStorage.getItem('watchlist').split(',') || [
+      'bitcoin',
+      'ethereum',
+      'theter',
+    ]
+  );
   const deleteCoin = coin => {
     setWatchList(watchlist.filter(e => e !== coin));
   };
+
+  useEffect(() => {
+    localStorage.setItem('watchlist', watchlist);
+  }, [watchlist]);
 
   const addCoin = coin => {
     if (watchlist.indexOf(coin) === -1) {
