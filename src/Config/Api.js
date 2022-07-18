1531ebdb288d115be99beaa6cb5c30d9dc65c984
Luis Carlos <roxisxprol@gmail.com>
2022-07-18 10:27:23 -0300

    Upadte Link Production

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -4,8 +4,5 @@ export const Lista = (currency) =>
 export const MoedaUnica = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
-export const Grafico = (id, days = 365, currency) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
-
 export const MoedaAlta = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
